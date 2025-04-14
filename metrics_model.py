9bc4c21bebbfd6bd3c24f148f61c47af6325ef5d
Daniyal Saeed <73713409+dsaeedModelOp@users.noreply.github.com>
2025-04-14 09:24:02 -0400

    Update metrics_model.py

--- metrics_model.py
+++ metrics_model.py
@@ -1,8 +1,9 @@
 import sys as sys
 
-# modelop.metrics
-def dict_metrics(datum):
-	yield {
-		"foo": 1,
-		"bar": "test result"
-	}
+
+# modelop.score
+def action(datum):
+	sys.stdout.flush()
+	print(datum)
+
+	yield datum
