3803583754f6ed9666c420f431b8140608933563
Daniyal Saeed <73713409+dsaeedModelOp@users.noreply.github.com>
2025-04-14 13:48:31 -0400

    Update metrics_model.py

--- metrics_model.py
+++ metrics_model.py
@@ -1,10 +1,7 @@
-import sys as sys
-
-
 # modelop.score
-def action(datum):
-	print("Test")
-	
-	print(datum)
-
-	yield datum
+def score(endpoint_input):
+    print("Score Called")
+    print(str("st_val: " +endpoint_input["st_val"]))
+    print(str("end_val: " + endpoint_input["end_val"]))
+    print("Score thread returned")
+    yield int(endpoint_input["end_val"]) - int(endpoint_input["st_val"])
